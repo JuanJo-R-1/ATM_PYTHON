1d34b0e949a8dcaa762fbf8bb7f0c38bc3e016d9
JuanJo <riverocamargojuanjose@gmail.com>
2025-05-27 13:02:09 -0500

    Add files via upload

--- list.py
+++ list.py
@@ -1,33 +1,17 @@
-palabras = []
+useful = ("pencil","eraser","rule","notebook","pen")
+useful=[]
+while True:
+ print(useful)
+ print("0. Pencil")
+ print("1. eraser")
+ print("2. rule")
+ print("3. notebook")
+ print("4. Pen")
 
-palabras.append("Python")
-palabras.append(5)
-palabras.append(True)
-palabras.append(input("ingrese un descripcion: "))
-
-palabras[0, "ID: 2"]
-
-print("Por elementos:")
-for item in palabras:
-    print(item)
-
-palabras[3]=False
-
-print("Por elementos despues del update:")
-for item in palabras:
-    respuesta=input(f"Quiere actualizar el valor de: {item}\ns\\n")
-    if respuesta=="s":
-        # palabras[x] = ""
-        item=input("ingrese el nuevo valor: ")
-    print(f"Valor nuevo: {item}")
-
-print("Por elementos final:")
-for item in palabras:
-    print(item)
-
-
-#for indice in range(len(palabras)):
-#    print(indice)
-#    print(type(indice))
-#    print(palabras(indice))
-#    print(type(palabras[indice]))
+ print("do you wanna add a new item?")
+ input()
+ if "yes":
+      print("wich item do you wanna add?")
+      useful.extend([input])
+ elif "no":
+      break
