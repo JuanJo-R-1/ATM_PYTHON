f8c77fcadc988f2fd10a284729798de15ba2d757
JuanJo <riverocamargojuanjose@gmail.com>
2025-05-27 13:01:44 -0500

    Add files via upload

---
+++ school-list.py
@@ -0,0 +1,10 @@
+while True:
+ print(useful)
+ print("1. Mostrar los productos")
+ print("2. Listar los productos")
+ print("3. actualizar un producto")
+ print("4. eliminar un producto")
+ print("5. Finalizar")
+
+ opcion=int(input("selecciona una opcion del menu: \n"))
+   if opcion==1:
